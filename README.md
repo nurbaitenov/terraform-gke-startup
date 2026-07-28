8a9f7513a34ebd1dab52420f669c950d64184922
nurbaitenov <baitenovnur@gmail.com>
2026-07-28 07:33:58 +0000

    added helm

--- README.md
+++ README.md
@@ -6,3 +6,10 @@
 gcloud container clusters get-credentials gke-cluster \
   --zone us-central1-a \
   --project project-eff7c2ad-c9a5-425e-96b
+
+
+# Helm installation
+curl -LO https://get.helm.sh/helm-v3.19.0-linux-amd64.tar.gz
+tar -zxvf helm-v3.19.0-linux-amd64.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin/helm
+helm version
--- provider.tf
+++ provider.tf
@@ -6,10 +6,28 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 6.0"
     }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+
+    helm = {
+      source = "hashicorp/helm"
+    }
   }
 }
 
 provider "google" {
   project = var.project_id
   region  = var.region
+}
+
+
+provider "helm" {
+  kubernetes {
+    host  = google_container_cluster.gke.endpoint
+    token = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(
+      google_container_cluster.gke.master_auth[0].cluster_ca_certificate
+    )
+  }
 }
