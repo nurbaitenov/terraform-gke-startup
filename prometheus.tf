3fa3ea97112d5f5dfeacf68dda84c7fd725d7ecc
nurbaitenov <baitenovnur@gmail.com>
2026-07-28 07:51:10 +0000

    added helm provider promethues

---
+++ prometheus.tf
@@ -0,0 +1,12 @@
+resource "helm_release" "prometheus" {
+
+  name = "prometheus"
+
+  repository = "https://prometheus-community.github.io/helm-charts"
+
+  chart = "kube-prometheus-stack"
+
+  namespace = "monitoring"
+
+  create_namespace = true
+}
--- provider.tf
+++ provider.tf
@@ -22,10 +22,24 @@ provider "google" {
 }
 
 
+provider "kubernetes" {
+  host = "https://${google_container_cluster.gke.endpoint}"
+
+  token = data.google_client_config.default.access_token
+
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.gke.master_auth[0].cluster_ca_certificate
+  )
+}
+
+data "google_client_config" "default" {}
+
 provider "helm" {
-  kubernetes {
-    host  = google_container_cluster.gke.endpoint
+  kubernetes = {
+    host = "https://${google_container_cluster.gke.endpoint}"
+
     token = data.google_client_config.default.access_token
+
     cluster_ca_certificate = base64decode(
       google_container_cluster.gke.master_auth[0].cluster_ca_certificate
     )
