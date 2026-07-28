874aabe63f20c95790afdf32e1ccb6b31dff7e74
nurbaitenov <baitenovnur@gmail.com>
2026-07-28 10:01:27 +0000

    added

---
+++ apis.tf
@@ -0,0 +1,6 @@
+resource "google_project_service" "secretmanager" {
+  project = var.project_id
+  service = "secretmanager.googleapis.com"
+
+  disable_on_destroy = false
+}
--- prometheus.tf
+++ prometheus.tf
@@ -15,4 +15,45 @@ resource "helm_release" "prometheus" {
       value = "LoadBalancer"
     }
   ]
+
+  values = [
+    yamlencode({
+      grafana = {
+        adminUser     = "admin"
+        adminPassword = data.google_secret_manager_secret_version.grafana.secret_data
+      }
+    })
+  ]
+}
+
+# create random passw for grafana access
+resource "random_password" "grafana" {
+  length  = 24
+  special = true
+}
+
+# store secret to Google Secret Manager
+resource "google_secret_manager_secret" "grafana" {
+  secret_id = "grafana-admin-password"
+
+  replication {
+    auto {}
+  }
+
+  depends_on = [
+    google_project_service.secretmanager
+  ]
+}
+
+resource "google_secret_manager_secret_version" "grafana" {
+  secret      = google_secret_manager_secret.grafana.id
+  secret_data = random_password.grafana.result
+}
+
+data "google_secret_manager_secret_version" "grafana" {
+  secret = google_secret_manager_secret.grafana.secret_id
+
+  depends_on = [
+    google_secret_manager_secret_version.grafana
+  ]
 }
