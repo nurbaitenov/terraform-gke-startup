911ba19f6b96a844bccf7df61b30a4f4c6b497ce
nurbaitenov <baitenovnur@gmail.com>
2026-07-29 11:20:48 +0000

    added

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source = "../module/network"
+  source = "./modules/network"
 
   project_id  = var.project_id
   region       = var.region
--- terraform/modules/network/output.tf
+++
@@ -1,7 +0,0 @@
-output "network_name" {
-  value = google_compute_network.vpc.name
-}
-
-output "subnet_name" {
-  value = google_compute_subnetwork.subnet.name
-}
