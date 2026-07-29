d22b2f5853ee4b202f670e8df186e7c5c3f24879
nurbaitenov <baitenovnur@gmail.com>
2026-07-29 09:08:41 +0000

    added

--- sonarqube.tf
+++ sonarqube.tf
@@ -1,52 +1,52 @@
-# resource "helm_release" "sonarqube" {
+resource "helm_release" "sonarqube" {
 
-#   name       = "sonarqube"
-#   repository = "https://SonarSource.github.io/helm-chart-sonarqube"
-#   chart      = "sonarqube"
+  name       = "sonarqube"
+  repository = "https://SonarSource.github.io/helm-chart-sonarqube"
+  chart      = "sonarqube"
 
-#   namespace        = "sonarqube"
-#   create_namespace = true
+  namespace        = "sonarqube"
+  create_namespace = true
 
-#   depends_on = [
-#     google_sql_database.sonarqube,
-#     google_sql_user.sonarqube,
-#     random_password.sonarqube_monitoring,
-#     google_sql_user.sonarqube
-#   ]
+  depends_on = [
+    google_sql_database.sonarqube,
+    google_sql_user.sonarqube,
+    random_password.sonarqube_monitoring,
+    google_sql_user.sonarqube
+  ]
 
-#   values = [
-#     yamlencode({
+  values = [
+    yamlencode({
 
-#       community = {
-#         enabled = true
-#       }
+      community = {
+        enabled = true
+      }
 
-#       monitoringPasscode = random_password.sonarqube_monitoring.result
+      monitoringPasscode = random_password.sonarqube_monitoring.result
 
-#       postgresql = {
-#         enabled = false
-#       }
+      postgresql = {
+        enabled = false
+      }
 
-#       jdbcOverwrite = {
-#         enabled = true
+      jdbcOverwrite = {
+        enabled = true
 
-#         jdbcUrl = "jdbc:postgresql://${google_sql_database_instance.postgres.public_ip_address}:5432/sonarqube"
+        jdbcUrl = "jdbc:postgresql://${google_sql_database_instance.postgres.public_ip_address}:5432/sonarqube"
 
-#         jdbcUsername = google_sql_user.sonarqube.name
+        jdbcUsername = google_sql_user.sonarqube.name
 
-#         jdbcPassword = random_password.postgres.result
-#       }
-#     })
-#   ]
-# }
+        jdbcPassword = random_password.postgres.result
+      }
+    })
+  ]
+}
 
-# resource "google_sql_user" "sonarqube" {
-#   instance = google_sql_database_instance.postgres.name
-#   name     = "sonarqube"
-#   password = random_password.postgres.result
-# }
+resource "google_sql_user" "sonarqube" {
+  instance = google_sql_database_instance.postgres.name
+  name     = "sonarqube"
+  password = random_password.postgres.result
+}
 
-# resource "random_password" "sonarqube_monitoring" {
-#   length  = 32
-#   special = false
-# }
+resource "random_password" "sonarqube_monitoring" {
+  length  = 32
+  special = false
+}
